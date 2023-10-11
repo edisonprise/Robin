219e51461328759d4588c124015a3a29aedb0af7
Edinson <edisonprise@hotmail.com>
2023-10-11 16:53:56 -0500

    final

--- Bracket_Results.js
+++ Bracket_Results.js
@@ -24,7 +24,7 @@ function Submit_Group_3_Function() {
   let team3_bracket = document.querySelectorAll(".bracket_names")[2];
   let team4_bracket = document.querySelectorAll(".bracket_names")[3];
 
-  let team1_input_forBracket = document.querySelectorAll(".Team_Name")[0]
+  let team1_input_forBracket = document.querySelectorAll(".Team_Name")[0];
   let team2_input_forBracket = document.querySelectorAll(".Team_Name")[1];
   let team3_input_forBracket = document.querySelectorAll(".Team_Name")[2];
   let team4_input_forBracket = document.querySelectorAll(".Team_Name")[3];
@@ -55,43 +55,42 @@ function submit_semi_Function() {
   /* let Result_finals_1 = document.querySelectorAll(".result_class")[0].value;
 let Result_finals_2 = document.querySelectorAll(".result_class")[1].value; */
 
+  /*-------------------------------------------------------------*/
 
-/*-------------------------------------------------------------*/
+  let team1_bracket_forbracket = document.querySelectorAll(".Team_Name")[0];
+  let team2_bracket_forbracket = document.querySelectorAll(".Team_Name")[1];
+  let team3_bracket_forbracket = document.querySelectorAll(".Team_Name")[2];
+  let team4_bracket_forbracket = document.querySelectorAll(".Team_Name")[3];
 
-let team1_bracket_forbracket = document.querySelectorAll('.Team_Name')[0]
-let team2_bracket_forbracket = document.querySelectorAll(".Team_Name")[1];
-let team3_bracket_forbracket = document.querySelectorAll(".Team_Name")[2];
-let team4_bracket_forbracket = document.querySelectorAll(".Team_Name")[3];
+  let team1_bracket = document.querySelectorAll(".bracket_names_2")[0];
+  let team2_bracket = document.querySelectorAll(".bracket_names_2")[1];
 
-let team1_bracket = document.querySelectorAll('.bracket_names_2')[0]
-let team2_bracket = document.querySelectorAll(".bracket_names_2")[1];
+  let team1_bracket_semi = document.querySelectorAll(".bracket_names")[0];
+  let team2_bracket_semi = document.querySelectorAll(".bracket_names")[1];
+  let team3_bracket_semi = document.querySelectorAll(".bracket_names")[2];
+  let team4_bracket_semi = document.querySelectorAll(".bracket_names")[3];
 
-let team1_bracket_semi = document.querySelectorAll('.bracket_names')[0]
-let team2_bracket_semi = document.querySelectorAll(".bracket_names")[1];
-let team3_bracket_semi = document.querySelectorAll(".bracket_names")[2];
-let team4_bracket_semi = document.querySelectorAll(".bracket_names")[3];
+  let result_01_semi = document.getElementById("result_G1_1");
+  let result_02_semi = document.getElementById("result_G2_2");
+  let result_03_semi = document.getElementById("result_G2_1");
+  let result_04_semi = document.getElementById("result_G1_2");
 
-let result_01_semi = document.getElementById('result_G1_1')
-let result_02_semi = document.getElementById("result_G2_2");
-let result_03_semi = document.getElementById("result_G2_1");
-let result_04_semi = document.getElementById("result_G1_2");
+  let team_input_bracket1 = document.querySelectorAll(".bracket_Input")[0];
+  let team_input_bracket2 = document.querySelectorAll(".bracket_Input")[1];
+  let team_input_bracket3 = document.querySelectorAll(".bracket_Input")[2];
+  let team_input_bracket4 = document.querySelectorAll(".bracket_Input")[3];
 
-let team_input_bracket1 = document.querySelectorAll('.bracket_Input')[0]
-let team_input_bracket2 = document.querySelectorAll('.bracket_Input')[1]
-let team_input_bracket3 = document.querySelectorAll('.bracket_Input')[2]
-let team_input_bracket4 = document.querySelectorAll('.bracket_Input')[3]
-
-team_input_bracket1.textContent = result_01_semi.value
-team_input_bracket2.textContent = result_02_semi.value;
-team_input_bracket3.textContent = result_03_semi.value;
-team_input_bracket4.textContent = result_04_semi.value;
-//////////////////////////////////////////////////////////////////////////////
+  team_input_bracket1.textContent = result_01_semi.value;
+  team_input_bracket2.textContent = result_02_semi.value;
+  team_input_bracket3.textContent = result_03_semi.value;
+  team_input_bracket4.textContent = result_04_semi.value;
+  //////////////////////////////////////////////////////////////////////////////
   if (Result1 > Result2) {
     team_finals_input1.textContent = team1_input.textContent;
 
-    team1_bracket.textContent = team1_bracket_forbracket.textContent
-    team1_bracket_semi.className = "bracket_names winner"
-    team2_bracket_semi.className = "bracket_names"
+    team1_bracket.textContent = team1_bracket_forbracket.textContent;
+    team1_bracket_semi.className = "bracket_names winner";
+    team2_bracket_semi.className = "bracket_names";
   } else if (Result1 < Result2) {
     team_finals_input1.textContent = team2_input.textContent;
 
@@ -132,51 +131,55 @@ let submit_finals = document.getElementById("submit_finals");
 submit_finals.addEventListener("click", submit_finals_Function);
 
 function submit_finals_Function() {
-
   /*Top*/
-  let team_finals1 = document.getElementsByClassName("Team_Name2")[0]
-  let team_finals2 = document.getElementsByClassName("Team_Name2")[1]
+  let team_finals1 = document.getElementsByClassName("Team_Name2")[0];
+  let team_finals2 = document.getElementsByClassName("Team_Name2")[1];
 
-  let team_finals_result1 = document.getElementsByClassName("result_class2")[0].value
-  let team_finals_result2 = document.getElementsByClassName("result_class2")[1].value
+  let team_finals_result1 =
+    document.getElementsByClassName("result_class2")[0].value;
+  let team_finals_result2 =
+    document.getElementsByClassName("result_class2")[1].value;
 
   /*Bottom*/
-let team_finals_name_bracket1 = document.getElementsByClassName("bracket_names_2")[0]
-let team_finals_name_bracket2 = document.getElementsByClassName("bracket_names_2")[1]
+  let team_finals_name_bracket1 =
+    document.getElementsByClassName("bracket_names_2")[0];
+  let team_finals_name_bracket2 =
+    document.getElementsByClassName("bracket_names_2")[1];
 
-let team_finals_result_bracket1 = document.getElementsByClassName("bracket_Input_2")[0]
-let team_finals_result_bracket2 = document.getElementsByClassName("bracket_Input_2")[1]
+  let team_finals_result_bracket1 =
+    document.getElementsByClassName("bracket_Input_2")[0];
+  let team_finals_result_bracket2 =
+    document.getElementsByClassName("bracket_Input_2")[1];
 
-/*Final Box*/
+  /*Final Box*/
 
-let finals_object = document.getElementsByClassName("bracket_names_3")[0]
+  let finals_object = document.getElementsByClassName("bracket_names_3")[0];
 
-team_finals_result_bracket1.textContent = team_finals_result1
-team_finals_result_bracket2.textContent = team_finals_result2
+  team_finals_result_bracket1.textContent = team_finals_result1;
+  team_finals_result_bracket2.textContent = team_finals_result2;
 
   if (team_finals_result1 > team_finals_result2) {
-    finals_object.textContent = team_finals1.textContent
+    finals_object.textContent = team_finals1.textContent;
 
-    team_finals_name_bracket1.className = "bracket_names_2 winner"
-    team_finals_name_bracket2.className = "bracket_names_2"
+    team_finals_name_bracket1.className = "bracket_names_2 winner";
+    team_finals_name_bracket2.className = "bracket_names_2";
 
-    finals_object.className = "bracket_names_3 winner_gold"
+    finals_object.className = "bracket_names_3 winner_gold";
   } else if (team_finals_result1 < team_finals_result2) {
-    finals_object.textContent = team_finals2.textContent
+    finals_object.textContent = team_finals2.textContent;
 
-    team_finals_name_bracket1.className = "bracket_names_2"
-    team_finals_name_bracket2.className = "bracket_names_2 winner"
+    team_finals_name_bracket1.className = "bracket_names_2";
+    team_finals_name_bracket2.className = "bracket_names_2 winner";
 
-    finals_object.className = "bracket_names_3 winner_gold"
+    finals_object.className = "bracket_names_3 winner_gold";
   } else if (team_finals_result1 === team_finals_result2) {
-    finals_object.textContent = "Winner Finals"
+    finals_object.textContent = "Winner Finals";
 
-    team_finals_name_bracket1.className = "bracket_names_2"
-    team_finals_name_bracket2.className = "bracket_names_2"
+    team_finals_name_bracket1.className = "bracket_names_2";
+    team_finals_name_bracket2.className = "bracket_names_2";
 
-    finals_object.className = "bracket_names_3"
+    finals_object.className = "bracket_names_3";
   }
-
 }
 
 /* function Submit_Group_3_Function() {
